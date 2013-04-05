e43fc33a9ea426e19b7386ee36ab8b0f19459e52
Tak Tran <contact@tutaktran.com>
2013-04-05 13:31:28 +0100

    Create App.init function

--- public/js/ideas-tennis.js
+++ public/js/ideas-tennis.js
@@ -6,22 +6,45 @@
     userEl: $("#user"),
     historyEl: $("#history"),
     thoughtEl: $("#thought"),
-    thoughtSubmitEl: $("#thought-submit")
+    thoughtSubmitEl: $("#thought-submit"),
+
+    init: function () {
+      // Enable pusher logging - don't include this in production
+      Pusher.log = function(message) {
+        if (window.console && window.console.log) window.console.log(message);
+      };
+
+      var pusher = new Pusher('48376e31f1c1d5d2e85c');
+      var channel = pusher.subscribe('ideas');
+      channel.bind('idea', function(data) {
+        console.log(data);
+        var userHtml = "<span class='user'>" + data.user + "</span>",
+          message = "<p class='" + data.side + "'>" + data.thought + userHtml + "</p>";
+        App.historyEl.append(message);
+      });
+
+      // Submit thought on click of button
+      App.thoughtSubmitEl.click(function (e) {
+        e.preventDefault();
+        submitThought();
+      });
+
+      // Pressing enter submits form
+      App.thoughtEl.keypress(function (e) {
+        if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+          submitThought();
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      // Random user name
+      var randomNumber = Math.floor(Math.random() * 10000) + 1,
+        randomUserName = "user" + randomNumber;
+      App.userEl.val(randomUserName);
+    }
   }
-
-  // Enable pusher logging - don't include this in production
-  Pusher.log = function(message) {
-    if (window.console && window.console.log) window.console.log(message);
-  };
-
-  var pusher = new Pusher('48376e31f1c1d5d2e85c');
-  var channel = pusher.subscribe('ideas');
-  channel.bind('idea', function(data) {
-    console.log(data);
-    var userHtml = "<span class='user'>" + data.user + "</span>",
-      message = "<p class='" + data.side + "'>" + data.thought + userHtml + "</p>";
-    App.historyEl.append(message);
-  });
 
   function getSide() {
     var activeSide = $("#side .active").first(),
@@ -55,23 +78,5 @@
     App.thoughtEl.val("");
   }
 
-  App.thoughtSubmitEl.click(function (e) {
-    e.preventDefault();
-    submitThought();
-  });
-
-  // Pressing enter submits form
-  App.thoughtEl.keypress(function (e) {
-    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-      submitThought();
-      return false;
-    } else {
-      return true;
-    }
-  });
-
-  // Random user name
-  var randomNumber = Math.floor(Math.random() * 10000) + 1,
-    randomUserName = "user" + randomNumber;
-  App.userEl.val(randomUserName);
+  App.init();
 })();
